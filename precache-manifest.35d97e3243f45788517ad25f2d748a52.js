2b0408b68199e3b940118f899e5aef76faa04ce3
ejdweck <evanjordandweck@gmail.com>
2019-07-07 15:29:42 -0700

    Updates

---
+++ precache-manifest.35d97e3243f45788517ad25f2d748a52.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0d2a9716c1867a7b1258f709874527",
+    "url": "/watermelons/index.html"
+  },
+  {
+    "revision": "0fcb0ce346bcc839fbee",
+    "url": "/watermelons/static/css/main.0ac5b48d.chunk.css"
+  },
+  {
+    "revision": "41929a0b7c088b7c55ae",
+    "url": "/watermelons/static/js/2.330470ca.chunk.js"
+  },
+  {
+    "revision": "0fcb0ce346bcc839fbee",
+    "url": "/watermelons/static/js/main.5f2a4e16.chunk.js"
+  },
+  {
+    "revision": "de95d76ca921e6c2a902",
+    "url": "/watermelons/static/js/runtime~main.d7752476.js"
+  },
+  {
+    "revision": "d1800f2159003b952f7a687620d43af9",
+    "url": "/watermelons/static/media/blue-melon.d1800f21.svg"
+  },
+  {
+    "revision": "b0a39c3873fd1019d70c333278430620",
+    "url": "/watermelons/static/media/pink-melon.b0a39c38.svg"
+  },
+  {
+    "revision": "dee43b6fcd181e993ea953cede609e0b",
+    "url": "/watermelons/static/media/purple-melon.dee43b6f.svg"
+  },
+  {
+    "revision": "09ac3d33492febe2e72c52f305ed3e6b",
+    "url": "/watermelons/static/media/red-melon.09ac3d33.svg"
+  }
+]);
